f5ab403497ea879d441844446f73746f5a9847fd
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-20 18:01:31 +0200

    add: output message and layout(wip)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
 // LOGICA DEL PROGRAMMA
+let number;
+let numberType;
+let fizzOrBuzz;
+let result = "";
+
 for (i = 1; i <= 100; i++) {
-    console.log(i);
-    let numberType;
+    number = i;
+    console.log(number);
+    
     if (i % 2 === 0) {
-        numberType = "even";
+        numberType = "ms_even";
         console.log(numberType);
-    } else {
-        numberType = "odd";
+    } else if (i % 2 === 1) {
+        numberType = "ms_odd";
         console.log(numberType);
     }
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        fizzOrBuzz = "FizzBuzz";
+    if (number % 3 === 0 && number % 5 === 0) {
+        fizzOrBuzz = "ms_fizz-buzz";
         console.log(fizzOrBuzz);
-    } else if (i % 3 === 0) {
-        fizzOrBuzz = "Fizz";
+    } else if (number % 3 === 0) {
+        fizzOrBuzz = "ms_fizz";
         console.log(fizzOrBuzz);
-    } else if (i % 5 === 0) {
-        fizzOrBuzz = "Buzz";
+    } else if (number % 5 === 0) {
+        fizzOrBuzz = "ms_buzz";
         console.log(fizzOrBuzz);
+    } else {
+        fizzOrBuzz = "";
     }
+    
+
+    // PREPARAZIONE MESSAGGIO
+    result += `<div class="ms_col ${numberType} ${fizzOrBuzz}"><span class="d-inline-block ms_text fw-semibold fs-6">${number}</span><span class="d-inline-block ms_text fw-semibold fs-6">${numberType}</span><span class="d-inline-block ms_text fw-semibold fs-6">${fizzOrBuzz}</span></div>`;
+    console.log(result);
+
+
+    // // OUTPUT MESSAGGIO
+    const rowElement = document.querySelector(".row");
+    rowElement.innerHTML = result;
+
+
+
+
 }
 
-// PREPARAZIONE MESSAGGIO
-const result = numberType + fizzOrBuzz;
 
 
